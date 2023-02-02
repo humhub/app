19997657763bd2edb323278cf5fc16fa5821683e
Primož Ratej <primozratej@gmail.com>
2023-02-02 18:28:20 +0100

    Moved keys to const and change it to snakeCase

--- lib/util/providers.dart
+++ lib/util/providers.dart
@@ -5,6 +5,8 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:humhub/models/hum_hub.dart';
 import 'package:humhub/models/manifest.dart';
 
+import 'const.dart';
+
 class HumHubNotifier extends ChangeNotifier {
   final HumHub _humHubInstance;
 
@@ -36,17 +38,17 @@ class HumHubNotifier extends ChangeNotifier {
 
   _updateSafeStorage() async {
     final jsonString = json.encode(_humHubInstance.toJson());
-    String lastUrl = _humHubInstance.manifest != null ? _humHubInstance.manifest!.baseUrl : "";
-    await _storage.write(key: "hum_hub", value: jsonString);
-    await _storage.write(key: "hum_hub_last_url", value: lastUrl);
+    String lastUrl = _humHubInstance.manifest != null ? _humHubInstance.manifest!.baseUrl : await getLastUrl();
+    await _storage.write(key: StorageKeys.humhubInstance, value: jsonString);
+    await _storage.write(key: StorageKeys.lastInstanceUrl, value: lastUrl);
   }
 
   clearSafeStorage() async {
-    await _storage.delete(key: "hum_hub");
+    await _storage.delete(key: StorageKeys.humhubInstance);
   }
 
   Future<HumHub> getInstance() async {
-    var jsonStr = await _storage.read(key: "hum_hub");
+    var jsonStr = await _storage.read(key: StorageKeys.humhubInstance);
     HumHub humHub = jsonStr != null
         ? HumHub.fromJson(json.decode(jsonStr))
         : _humHubInstance;
@@ -54,7 +56,7 @@ class HumHubNotifier extends ChangeNotifier {
   }
 
   Future<String> getLastUrl() async {
-    var lastUrl = await _storage.read(key: "hum_hub_last_url");
+    var lastUrl = await _storage.read(key: StorageKeys.lastInstanceUrl);
     return lastUrl ?? "";
   }
 }
