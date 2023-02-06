81f8e835fb51c532bd5da5a301b2b0edc7b9ef28
Primož Ratej <primozratej@gmail.com>
2023-02-06 16:50:31 +0100

    ensureInitialized and implement onWillPop

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:humhub/util/redirect_handler.dart';
 
-void main() {
+main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: MyApp()));
 }
 
