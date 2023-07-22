d47c4b6dab38ef0602dcacf1b049a30b0c8bceb5
primozratej <primozratej@gmail.com>
2023-07-22 15:26:55 +0200

    Change "log in to" to "log into"

--- lib/util/const.dart
+++ lib/util/const.dart
@@ -16,11 +16,11 @@ class Locales {
   static String helpThirdPar =
       'The HumHub app is intuitive to use and allows you to access your network from anywhere. When notifications are enabled, you will also receive push notifications about important information within your network and can use the app to keep up to date with any relevant news at any time.';
   static String helpForthPar =
-      'HumHub networks are basically private (internal to the organization) communication platforms. It is only possible to log in to existing networks for which you have corresponding login data. Organizations or companies can of course set up and run their own HumHub network at any time. All information can be found at';
+      'HumHub networks are basically private (internal to the organization) communication platforms. It is only possible to log into existing networks for which you have corresponding login data. Organizations or companies can of course set up and run their own HumHub network at any time. All information can be found at';
 
   static String howToConnectTitle = "How can I connect my network?";
   static String howToConnectFirstPar =
-      "There are many thousands of networks worldwide. Therefore, to log in to your existing network, you need the exact URL of your individual network.";
+      "There are many thousands of networks worldwide. Therefore, to log into your existing network, you need the exact URL of your individual network.";
   static String howToConnectSecPar = "Your URL can be found in the address bar of your browser in the login area of your network.";
   static String howToConnectThirdPar = "Please contact your administrator or network operator if you do not know your URL or login details.";
 
