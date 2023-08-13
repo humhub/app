e6d86e79faa5152591919419d62c2e67507d80c3
primozratej <primozratej@gmail.com>
2023-08-13 18:54:56 +0200

    Push redirect to correct page
    Handle app sleeping, app open but not in webView (opener), opened in webView. Move Push provider to higher level.

--- lib/util/api_provider.dart
+++ lib/util/api_provider.dart
@@ -1,22 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:humhub/models/manifest.dart';
 
 class APIProvider {
   final WidgetRef _ref;
 
   APIProvider(this._ref);
-  
+
   static Map<String, String> get jsonContent => {
-    'content-type': 'application/json',
-    'accept': 'application/json',
-  };
+        'content-type': 'application/json',
+        'accept': 'application/json',
+      };
 
   static APIProvider of(WidgetRef ref) => APIProvider(ref);
 
   Future<AsyncValue<T>> request<T>(
-      Future<T> Function(Dio dio) fetcher, {
-        Dio? dio,
-      }) async {
+    Future<T> Function(Dio dio) fetcher, {
+    Dio? dio,
+  }) async {
     dio = dio ?? _ref.read(dioProvider);
     try {
       final value = await fetcher(dio!);
@@ -32,4 +33,19 @@ class APIProvider {
     ));
     return api;
   });
+
+  static Future<AsyncValue<Manifest>> requestBasic(
+    Future Function(Dio dio) fetcher, {
+    Dio? dio,
+  }) async {
+    dio = Dio(BaseOptions(
+      headers: APIProvider.jsonContent,
+    ));
+    try {
+      final value = await fetcher(dio);
+      return AsyncValue.data(value);
+    } catch (err, stackTrace) {
+      return AsyncValue.error(err, stackTrace);
+    }
+  }
 }
