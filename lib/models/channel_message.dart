c0ebfa4c6ca2ea3ae4f55ffd1ffa4dd56669aff4
primozratej <primozratej@gmail.com>
2023-08-22 21:53:23 +0200

    Set unregisterFcmDevice action

--- lib/models/channel_message.dart
+++ lib/models/channel_message.dart
@@ -12,7 +12,7 @@ class ChannelMessage {
     if (type == "showOpener") return ChannelAction.showOpener;
     if (type == "hideOpener") return ChannelAction.hideOpener;
     if (type == "registerFcmDevice") return ChannelAction.registerFcmDevice;
-    if (type == "unregisterFcmDevice") return ChannelAction.registerFcmDevice;
+    if (type == "unregisterFcmDevice") return ChannelAction.unregisterFcmDevice;
     if (type == "updateNotificationCount") return ChannelAction.updateNotificationCount;
 
     return ChannelAction.none;
