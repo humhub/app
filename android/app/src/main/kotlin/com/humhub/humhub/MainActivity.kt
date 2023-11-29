5bdf97982d671abdeeb0394f4102b46a141a2545
primozratej <primozratej@gmail.com>
2023-11-29 20:55:16 +0700

    Enable intent for images

--- android/app/src/main/kotlin/com/humhub/humhub/MainActivity.kt
+++ android/app/src/main/kotlin/com/humhub/humhub/MainActivity.kt
@@ -5,12 +5,12 @@ import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 
 class MainActivity: FlutterActivity() {
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         if (intent.getIntExtra("org.chromium.chrome.extra.TASK_ID", -1) == this.taskId) {
             this.finish()
             intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
         super.onCreate(savedInstanceState)
-    }*/
+    }
 }
