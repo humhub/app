68537abfd6527f37337eea3039591e209c236f15
primozratej <primozratej@gmail.com>
2024-03-11 11:15:00 +0700

    Add tests for community, sometest12345 and test.cuzy.app

--- test/opener_test.dart
+++ test/opener_test.dart
@@ -1,44 +1,85 @@
-import 'dart:io';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:humhub/util/opener_controller.dart';
-import 'package:mockito/mockito.dart';
-
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-  }
-}
-
-class MockFlutterSecureStorage extends Mock implements FlutterSecureStorage {}
 
 void main() {
-  group('generatePossibleManifestsUrls', () {
-    Map<String, String> uriMap = {
-      "https://test.cuzy.app/humhub": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
-      "test.cuzy.app/humhub/": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
-      "test.cuzy.app/": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
-      "sometestproject12345.humhub.com": "https://sometestproject12345.humhub.com/manifest.json",
-      "sometestproject12345.humhub.com/humhub": "https://sometestproject12345.humhub.com/manifest.json",
-      "sometestproject12345.humhub.com/acc": "https://sometestproject12345.humhub.com/z/manifest.json",
-      "sometestproject12345.humhub.com/login": "https://sometestproject12345.humhub.com/a/manifest.json",
-    };
+  void testGroupOfURIs(Map<String, String> uriMap) {
+    List<String> failedExpectations = [];
 
-    test('Check URLs', () {
-      List<String> failedExpectations = [];
-
-      uriMap.forEach((key, value) {
-        List<String> generatedUrls = OpenerController.generatePossibleManifestsUrls(key);
-        if (!generatedUrls.contains(value)) {
-          failedExpectations.add("🐛 Opener URL $key generated:\n ${generatedUrls.toString()} list \n the expected value $value was not found");
-        }
-      });
-
-      if (failedExpectations.isNotEmpty) {
-        fail(failedExpectations.join("\n\n"));
+    uriMap.forEach((key, value) {
+      List<String> generatedUrls = OpenerController.generatePossibleManifestsUrls(key);
+      if (!generatedUrls.contains(value)) {
+        failedExpectations.add(
+            "🐛 Opener URL $key generated:\n ${generatedUrls.toString()} list \n the expected value $value was not found");
       }
     });
+
+    if (failedExpectations.isNotEmpty) {
+      fail(failedExpectations.join("\n\n"));
+    }
+  }
+
+  group('Generate possible Manifests.json URLs and check if exists', () {
+    /// [key] represents the opener dialog input string
+    /// [value] represents the actual manifest.json location
+
+    test('Check HumHub Community URLs', () {
+      Map<String, String> map = {
+        "https://community.humhub.com/": "https://community.humhub.com/manifest.json",
+        "https://community.humhub.com": "https://community.humhub.com/manifest.json",
+        "community.humhub.com/": "https://community.humhub.com/manifest.json",
+        "community.humhub.com": "https://community.humhub.com/manifest.json",
+        "community.humhub.com/some/more": "https://community.humhub.com/manifest.json",
+        "https://community.humhub.com/and/more": "https://community.humhub.com/manifest.json",
+      };
+      testGroupOfURIs(map);
+    });
+
+    test('Check sometestproject12345 URLs', () {
+      Map<String, String> map = {
+        "https://sometestproject12345.humhub.com/": "https://sometestproject12345.humhub.com/manifest.json",
+        "https://sometestproject12345.humhub.com": "https://sometestproject12345.humhub.com/manifest.json",
+        "sometestproject12345.humhub.com/": "https://sometestproject12345.humhub.com/manifest.json",
+        "sometestproject12345.humhub.com": "https://sometestproject12345.humhub.com/manifest.json",
+        "https://sometestproject12345.humhub.com/some/more": "https://sometestproject12345.humhub.com/manifest.json",
+        "https://sometestproject12345.humhub.com/manifest.json":
+            "https://sometestproject12345.humhub.com/manifest.json",
+      };
+      testGroupOfURIs(map);
+    });
+
+    test('Check some sometestproject12345 URLs', () {
+      Map<String, String> map = {
+        "https://sometestproject12345.humhub.com/": "https://sometestproject12345.humhub.com/manifest.json",
+        "https://sometestproject12345.humhub.com": "https://sometestproject12345.humhub.com/manifest.json",
+        "sometestproject12345.humhub.com/": "https://sometestproject12345.humhub.com/manifest.json",
+        "sometestproject12345.humhub.com": "https://sometestproject12345.humhub.com/manifest.json",
+        "https://sometestproject12345.humhub.com/some/more": "https://sometestproject12345.humhub.com/manifest.json",
+        "https://sometestproject12345.humhub.com/manifest.json":
+            "https://sometestproject12345.humhub.com/manifest.json",
+      };
+      testGroupOfURIs(map);
+    });
+
+    test('Check some test.cuzy.app URLs', () {
+      Map<String, String> map = {
+        "https://test.cuzy.app/humhub": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
+        "test.cuzy.app/humhub": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
+        "test.cuzy.app/humhub/some": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
+        "test.cuzy.app/humhub": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
+        "https://test.cuzy.app/humhub/some": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
+      };
+      testGroupOfURIs(map);
+    });
+
+    /// TEMPLATE: copy this and change it for your use case
+    /*test('Check some HumHum instance URLs', () {
+      /// [key] represents the opener dialog input string
+      /// [value] represents the actual manifest.json location
+      Map<String, String> map = {
+        "https://test.cuzy.app/humhub": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
+        "test.cuzy.app/humhub": "https://test.cuzy.app/humhub/index.php?r=web%2Fpwa-manifest%2Findex",
+      };
+      testGroupOfURIs(map);
+    });*/
   });
 }
