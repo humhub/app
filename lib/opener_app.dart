f31450c4cf70f265be3256359d648037f9079bde
primozratej <primozratej@gmail.com>
2024-03-24 15:47:20 +0700

    Flavored

--- lib/opener_app.dart
+++ lib/opener_app.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:humhub/util/intent/intent_plugin.dart';
 import 'package:humhub/util/notifications/plugin.dart';
+import 'package:humhub/util/override_locale.dart';
 import 'package:humhub/util/push/push_plugin.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:humhub/util/router.dart';
 
 class OpenerApp extends ConsumerStatefulWidget {
@@ -18,22 +20,29 @@ class OpenerAppState extends ConsumerState<OpenerApp> {
     return IntentPlugin(
       child: NotificationPlugin(
         child: PushPlugin(
-          child: FutureBuilder<String>(
-            future: MyRouter.getInitialRoute(ref),
-            builder: (context, snap) {
-              if (snap.hasData) {
-                return MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  initialRoute: snap.data,
-                  routes: MyRouter.routes,
-                  navigatorKey: navigatorKey,
-                  theme: ThemeData(
-                    fontFamily: 'OpenSans',
-                  ),
-                );
-              }
-              return const SizedBox.shrink();
-            },
+          child: OverrideLocale(
+            builder: (overrideLocale) => Builder(
+              builder: (context) => FutureBuilder<String>(
+                future: MyRouter.getInitialRoute(ref),
+                builder: (context, snap) {
+                  if (snap.hasData) {
+                    return MaterialApp(
+                      debugShowCheckedModeBanner: false,
+                      initialRoute: snap.data,
+                      routes: MyRouter.routes,
+                      navigatorKey: navigatorKey,
+                      localizationsDelegates: AppLocalizations.localizationsDelegates,
+                      supportedLocales: AppLocalizations.supportedLocales,
+                      locale: overrideLocale,
+                      theme: ThemeData(
+                        fontFamily: 'OpenSans',
+                      ),
+                    );
+                  }
+                  return const SizedBox.shrink();
+                },
+              ),
+            ),
           ),
         ),
       ),
