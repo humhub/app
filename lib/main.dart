b2e990b89d71602cce62542f4e64a437106c4cd7
primozratej <primozratej@gmail.com>
2024-05-17 14:04:21 +0200

    SecureStorageService and split app logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:humhub/util/intent/intent_plugin.dart';
-import 'package:humhub/util/loading_provider.dart';
 import 'package:humhub/util/log.dart';
-import 'package:humhub/util/notifications/plugin.dart';
-import 'package:humhub/util/override_locale.dart';
-import 'package:humhub/util/push/push_plugin.dart';
-import 'package:humhub/util/router.dart';
+import 'package:humhub/util/storage_service.dart';
 import 'package:loggy/loggy.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'apps/opener_app.dart';
 
 main() async {
   Loggy.initLoggy(
     logPrinter: const GlobalLog(),
   );
   WidgetsFlutterBinding.ensureInitialized();
-  await clearSecureStorageOnReinstall();
+  await SecureStorageService.clearSecureStorageOnReinstall();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
-    runApp(const ProviderScope(child: MyApp()));
+    runApp(const ProviderScope(child: OpenerApp()));
   });
-}
-
-class MyApp extends ConsumerStatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends ConsumerState<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    clearSecureStorageOnReinstall();
-    return IntentPlugin(
-      child: NotificationPlugin(
-        child: PushPlugin(
-          child: OverrideLocale(
-            builder: (overrideLocale) => Builder(
-              builder: (context) => FutureBuilder<String>(
-                future: MyRouter.getInitialRoute(ref),
-                builder: (context, snap) {
-                  if (snap.hasData) {
-                    return MaterialApp(
-                      debugShowCheckedModeBanner: false,
-                      initialRoute: snap.data,
-                      routes: MyRouter.routes,
-                      navigatorKey: navigatorKey,
-                      localizationsDelegates: AppLocalizations.localizationsDelegates,
-                      supportedLocales: AppLocalizations.supportedLocales,
-                      locale: overrideLocale,
-                      builder: (context, child) => LoadingProvider(
-                        child: child!,
-                      ),
-                      theme: ThemeData(
-                        fontFamily: 'OpenSans',
-                      ),
-                    );
-                  }
-                  return const SizedBox.shrink();
-                },
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-clearSecureStorageOnReinstall() async {
-  String key = 'hasRunBefore';
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  bool hasRunBefore = prefs.getBool(key) ?? false;
-  if (!hasRunBefore) {
-    FlutterSecureStorage storage = const FlutterSecureStorage();
-    await storage.deleteAll();
-    prefs.setBool(key, true);
-  }
 }
