9c8aa2202b2d11c5eeac55d79dd9b5fd88e3dba0
primozratej <primozratej@gmail.com>
2024-05-28 18:55:06 +0200

    Remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:humhub/models/hum_hub.dart';
 import 'package:humhub/util/log.dart';
