b4e044a39def9f69fa9042e69bf727ab82f433bb
primozratej <primozratej@gmail.com>
2024-06-17 17:47:49 +0200

    Support .env asset

--- lib/util/const.dart
+++ lib/util/const.dart
@@ -8,8 +8,8 @@ class StorageKeys {
 class Assets {
   static String logo = "assets/images/logo.png";
   static String helpImg = "assets/images/help.png";
-  static String openerAnimationForward = "assets/opener_animation.riv";
-  static String openerAnimationReverse = "assets/opener_animation_reverse.riv";
+  static String openerAnimationForward = "assets/animations/opener_animation.riv";
+  static String openerAnimationReverse = "assets/animations/opener_animation_reverse.riv";
 }
 
 class HumhubTheme {
