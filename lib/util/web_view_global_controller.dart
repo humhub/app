478312890a23b213692071d25979dcd369e0cd99
primozratej <primozratej@gmail.com>
2024-09-10 20:36:10 +0200

    Update comments

--- lib/util/web_view_global_controller.dart
+++ lib/util/web_view_global_controller.dart
@@ -14,20 +14,17 @@ class WebViewGlobalController {
   /// Determines if a URL should open in a new browser window or within the current web view.
   ///
   /// - Opens in a new window if:
-  ///   - The URL is for file downloads ('file/download').
-  ///   - The URL is a user profile redirect (`/u` after base URL).
-  ///   - The URL is a space redirect (`/s` after base URL).
+  ///   - The URL is for file downloads (`file/file/download` after base URL).
+  ///   - The URL is a @username profile redirect (`/u` after base URL).
+  ///   - The URL is a @space redirect (`/s` after base URL).
   ///
   /// [ref] is reference to the app state.
   /// [url] is the URL to evaluate.
   /// @return `true` if the URL should open in a new window, `false` otherwise.
   static bool openCreateWindowInWebView(WidgetRef ref, String url) {
     String? baseUrl = ref.read(humHubProvider).manifest?.baseUrl;
-    // When app wants to open new window for file downloads
     if (url.startsWith('$baseUrl/file/file/download')) return true;
-    // When app wants to open new window tag redirects (@username)
     if (url.startsWith('$baseUrl/u')) return true;
-    // When app wants to open new window tag redirects (@space)
     if (url.startsWith('$baseUrl/s')) return true;
     return false;
   }
