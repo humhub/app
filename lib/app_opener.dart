fa70eca855e2eef8c41c8a711b8f999464fe827f
primozratej <primozratej@gmail.com>
2025-04-28 14:35:32 +0700

    Remove unused imports

--- lib/app_opener.dart
+++ lib/app_opener.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:humhub/util/const.dart';
