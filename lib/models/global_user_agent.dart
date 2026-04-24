a5e0c430ad3774c1f339199df528016d9501f857
primozratej <primozratej@gmail.com>
2026-04-24 12:39:11 +0200

    Define iPad user agent

--- lib/models/global_user_agent.dart
+++ lib/models/global_user_agent.dart
@@ -4,6 +4,8 @@ import 'dart:ui';
 class GlobalUserAgent {
   static const String _ios =
       "Mozilla/5.0 (iPhone; CPU iPhone OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/131.0.6778.73 Mobile/15E148 Safari/604.1";
+  static const String _ipad =
+      "Mozilla/5.0 (iPad; CPU OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/131.0.6778.73 Mobile/15E148 Safari/604.1";
   static const String _android =
       "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.81 Mobile Safari/537.36";
 
@@ -17,7 +19,7 @@ class GlobalUserAgent {
   static String? get value {
     if (Platform.isIOS) {
       if (_isTabletViewport) {
-        return null;
+        return _ipad;
       }
       return _ios;
     } else if (Platform.isAndroid) {
